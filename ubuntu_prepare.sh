d577e655f041dd0715dac7a5048a09cf601a798c
Manuel Calero <manuelcalero.xtendoo@gmail.com>
2020-11-17 11:02:30 +0100

    syncro

--- ubuntu_prepare.sh
+++ ubuntu_prepare.sh
@@ -30,9 +30,7 @@ sudo apt-get install \
     gnupg-agent \
     software-properties-common
 
-
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
 
 sudo apt-key fingerprint 0EBFCD88
 
@@ -40,7 +38,6 @@ sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-
 
 sudo apt-get update
 
@@ -65,7 +62,6 @@ sleep 1
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
-
 
 sleep 1
 
